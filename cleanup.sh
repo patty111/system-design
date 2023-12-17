c3fe0e00f5c625f648da43d9f8c824e2345a0f37
patty <patrick.celee@gmail.com>
2023-12-17 13:07:11 +0800

    cleanup:update cleanup script to list all TODO comments

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-autoflake --remove-all-unused-imports --remove-unused-variables --in-place --recursive .
+autoflake --remove-all-unused-imports --remove-unused-variables --in-place . --exclude=__init__.py
+
+# listing all TODO comments
+grep --color=always -rn --exclude-dir=env --exclude-dir=env-wsl --exclude=cleanup.sh "TODO" .
